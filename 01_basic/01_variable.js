463121800425ed0410b7b1c7d2b7541f1f9941ee
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-04 13:57:35 +0000

    var

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,18 @@
+/*
+Prefer not to use var kewords to declare the variables
+because of issues in block scope and functional scope
+
+
+*/
+const accountId=12232;
+let accountEmail="rushishete@gmail.com";
+var accountPassword="123456";
+accountCity="Jaypur";
+console.log(accountId,accountEmail,accountPassword,accountCity);
+// accountId=2; // not allowed
+console.log(accountId);
+accountEmail="hc@ch.com";
+accountPassword="34234";
+accountCity="Bengluru";
+console.log([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity]);

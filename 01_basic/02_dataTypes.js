dfcef6ec0c8a37e752eee8cc4941977e1d374dfb
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-04 14:16:06 +0000

    ht

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code newer versions
+
+//alert("hello");// we are using node js not browsers
+//alert(3+3);// we are using node js not browsers
+
+let name="hitesh"
+let age="14"
+let isLoggedIn=false
+
+//number =>2 to power 53
+//bigint =>
+//string => ""
+//bollean => true/false
+//null => stand alone value  / nul ek type he 
+//undefined =>   
+//symbol => unique
+
+// objects
+
+
+console.log(typeof age);
+console.log(typeof null);
+
+
+console.log(typeof undefined);

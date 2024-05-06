1f585ee9873075c8c913fda15a3a5b210e77893d
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 21:49:33 +0000

    Object Methods: JavaScript objects come with built-in methods like Object.keys(), Object.values(), and Object.entries()

---
+++ 03_basic/04_objectMethods.js
@@ -0,0 +1,8 @@
+/**
+ * Object Methods: JavaScript objects come with built-in methods like Object.keys(), Object.values(), and Object.entries()
+ * for working with object properties.
+ * Example:
+ */
+let keys = Object.keys(person);
+let values = Object.values(person);
+let entries = Object.entries(person);

ad7c35a7e2e83ba5b9bf7f38acf4fc2713f7df72
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 22:18:32 +0000

    Function Return

---
+++ 04_basic/06_ functionReturn.js
@@ -0,0 +1,8 @@
+/**
+ * Function Return: Functions in JavaScript can return a value using the return statement. The return statement stops the execution
+ * of the function and returns the specified value to the caller. If no return value is specified, the function returns undefined.
+ * Example:
+ */
+function add(a, b) {
+    return a + b;
+}

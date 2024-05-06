752f5fdd10206d2260189282f77d6541a001011d
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 21:46:10 +0000

    Object Literals

---
+++ 03_basic/01_objectLiterals.js
@@ -0,0 +1,12 @@
+/**
+ * Object Literals: The most common way to create objects in JavaScript is using object literals {}.
+ * Example:
+ */
+let person = {
+    name: "John",
+    age: 30,
+    isMarried: false,
+    greet: function() {
+        console.log("Hello!");
+    }
+};

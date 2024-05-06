c9d5868b4ffac5196247392167baacb67ca9ac4f
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 21:48:16 +0000

    Adding and Modifying Properties in js

---
+++ 03_basic/03_addingAndModifyingProperties.js
@@ -0,0 +1,6 @@
+/**
+ * Adding and Modifying Properties: Properties can be added or modified dynamically by assigning values to them.
+ * Example:
+ */
+person.city = "New York";  // Adding a new property
+person.age = 31;  // Modifying an existing property

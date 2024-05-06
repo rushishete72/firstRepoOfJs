e20bc6f16bc5478016eb28b254e59f64e559e448
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 21:55:48 +0000

    Object Spread Operator:

---
+++ 03_basic/08_objectSpreadOperator.js
@@ -0,0 +1,6 @@
+/**
+ * Object Spread Operator: The spread operator (...) can be used to create a new object by merging properties from existing
+ * objects or adding new properties.
+ * Example:
+ */
+let newObj = { ...person, city: "Los Angeles" };

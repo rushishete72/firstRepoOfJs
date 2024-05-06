4d65c026b872b893802e9031d357c58ee655a196
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 21:47:47 +0000

    Properties and Methods of objects in js

---
+++ 03_basic/02_ propertiesAndMethods.js
@@ -0,0 +1,9 @@
+/**
+ * Properties and Methods: Properties are the key-value pairs in an object, and methods are functions stored as object properties.
+ * Accessing Properties and Methods: Properties and methods of an object can be accessed using dot notation (object.property) or
+ * bracket notation (object['property']).
+ * Example:
+ */
+console.log(person.name);  // Dot notation
+console.log(person['age']);  // Bracket notation
+person.greet();  // Calling method

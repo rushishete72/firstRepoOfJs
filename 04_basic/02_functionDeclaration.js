3653e776bf9db6340f1ff5b998950e2fc5929cc3
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 22:11:41 +0000

    Function Declaration in js

---
+++ 04_basic/02_functionDeclaration.js
@@ -0,0 +1,9 @@
+/**
+ * Function Declaration: Functions can be declared using the function keyword followed by the function name, a list of parameters
+ * enclosed in parentheses, and the function body enclosed in curly braces. Function declarations are hoisted to the top of
+ * their scope, meaning they can be called before they are declared.
+ * Example:
+ */
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}

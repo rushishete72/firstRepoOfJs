863d6daab69336b07e755b3a1ccc3c42841822b7
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 21:53:06 +0000

    Object Destructuring

---
+++ 03_basic/07_objectDestructuring.js
@@ -0,0 +1,5 @@
+/**
+ * Object Destructuring: Object destructuring is a convenient way to extract multiple properties from an object into separate variables.
+ * Example:
+ */
+let { name, age } = person;

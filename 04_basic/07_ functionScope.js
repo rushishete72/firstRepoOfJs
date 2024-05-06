46cc93b24a7134d8eb4ac59f8e702c62fb3f6570
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 22:19:23 +0000

    Function Scope

---
+++ 04_basic/07_ functionScope.js
@@ -0,0 +1,5 @@
+/**
+ * Function Scope: Variables declared inside a function are scoped to that function and cannot be accessed from outside the function.
+ * This is known as function scope. Variables declared outside any function have global scope and can be accessed from anywhere in
+ * the code.
+ */

784f035c8796abdae2cb89895a25175f5706d387
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 22:10:46 +0000

    Definition

---
+++ 04_basic/01_functionDefinition.js
@@ -0,0 +1,5 @@
+/**
+ * Definition: Functions in JavaScript are blocks of reusable code that perform a specific task. They can take input parameters
+ * and return a value. Functions are first-class citizens in JavaScript, meaning they can be assigned to variables, passed as
+ * arguments to other functions, and returned from other functions.
+ */

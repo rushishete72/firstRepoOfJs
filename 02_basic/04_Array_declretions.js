dc1a1aebda1be77d50755818e315f406b012e864
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 21:07:11 +0000

    Array Declaration

---
+++ 02_basic/04_Array_declretions.js
@@ -0,0 +1,20 @@
+// Empty Array Declaration
+let emptyArray = [];
+
+// Array with Initial Elements
+let arrayWithElements = [1, 2, 3, 4, 5];
+
+// Using the Array Constructor
+let arrayConstructor = new Array();
+
+// Array Constructor with Size
+let arrayConstructorWithSize = new Array(5); // Creates an array with length 5
+
+// Array Constructor with Initial Elements
+let arrayConstructorWithElements = new Array('apple', 'banana', 'orange');
+
+// Using Array.from() method: Converts a string into an array of characters
+let fromMethod = Array.from('hello');
+
+// Using Array.of() method: Creates an array from given arguments
+let ofMethod = Array.of(1, 2, 3, 4, 5);

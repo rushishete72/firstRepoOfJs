f7c0a9f7b87dc33a3097af3a3634b7ad9b32b4a8
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 22:15:40 +0000

    Function Parameters:

---
+++ 04_basic/05_functionParameters.js
@@ -0,0 +1,9 @@
+/**
+ * Function Parameters: Functions in JavaScript can take zero or more parameters, which are specified inside the parentheses
+ * in the function declaration or expression. Parameters are placeholders for values that will be passed to the function when it
+ * is called.
+ * Example:
+ */
+function add(a, b) {
+    return a + b;
+}

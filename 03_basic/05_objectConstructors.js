2b958ea1feb61eafe2469795c4e59ed104939581
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 21:50:29 +0000

    Object Constructors: in js

---
+++ 03_basic/05_objectConstructors.js
@@ -0,0 +1,13 @@
+/**
+ * Object Constructors: JavaScript allows defining object constructors using functions. Constructors are templates for creating
+ * objects with similar properties and methods.
+ * Example:
+ */
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.greet = function() {
+        console.log("Hello!");
+    };
+}
+let person1 = new Person("Alice", 25);

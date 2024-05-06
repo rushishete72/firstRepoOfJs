962973cda62a9c17aba066ed7ee7179a90907da8
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-06 22:13:10 +0000

    Function Expression

--- 03_functionExpression.js
+++
@@ -1,9 +0,0 @@
-/**
- * Function Expression: Functions can also be created using function expressions, where a function is assigned to a variable.
- * Function expressions can be named or anonymous. Anonymous function expressions are often used as callback functions or
- * immediately invoked function expressions (IIFE).
- * Example:
- */
-let greet = function(name) {
-    console.log("Hello, " + name + "!");
-};
